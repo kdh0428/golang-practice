6daec66366d234a03f610d21a0b4bdcad551a6fc
stuck_idiot <unlimiter69@gmail.com>
2018-03-04 01:39:56 +0900

    Variables

---
+++ values/variables.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main(){
+    var a = "initial"
+    fmt.Println(a)
+
+    b, c := 1.0, 2.0
+    fmt.Println(b, c)
+
+    var d = true
+    fmt.Println(d)
+
+    var e int
+    fmt.Println(e)
+
+    f := "short"
+    fmt.Println(f)
+}
